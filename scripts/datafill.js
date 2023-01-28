ec26a814e2fd3e38b6ab7b736d294c8785df796f
sageparsnip <btysall@gmail.com>
2023-01-28 21:53:09 +0000

    Finalised Conditions

--- scripts/datafill.js
+++ scripts/datafill.js
@@ -120,6 +120,86 @@ function loadJSON(){
 	$.getJSON("data/conditions.json", function (data){
 		//JSON file loaded to var "data"
 		console.log("There are " + Object.keys(data.FullList).length + " conditions to be loaded");
+		for(var i = 0; i< Object.keys(data.Senses).length; i++){
+			$("#Senses").html(function(k, oldHTML){
+				//Modify the "Senses" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data.Senses[i].name + "</div>" +
+					"<div class='item-text'>" + data.Senses[i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Health & Dying"]).length; i++){
+			$("#HealthDying").html(function(k, oldHTML){
+				//Modify the "Health & Dying" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Health & Dying"][i].name + "</div>" +
+					"<div class='item-text'>" + data["Health & Dying"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Visibility"]).length; i++){
+			$("#Visibility").html(function(k, oldHTML){
+				//Modify the "Visibility" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Visibility"][i].name + "</div>" +
+					"<div class='item-text'>" + data["Visibility"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Physical State"]).length; i++){
+			$("#PhysicalState").html(function(k, oldHTML){
+				//Modify the "Physical State" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Physical State"][i].name + "</div>" +
+					"<div class='item-text'>" + data["Physical State"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Mental State"]).length; i++){
+			$("#MentalState").html(function(k, oldHTML){
+				//Modify the "Mental State" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Mental State"][i].name + "</div>" +
+					"<div class='item-text'>" + data["Mental State"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Action Economy"]).length; i++){
+			$("#ActionEconomy").html(function(k, oldHTML){
+				//Modify the "Action Economy" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Action Economy"][i].name + "</div>" +
+					"<div class='item-text'>" + data["Action Economy"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["NPC Regard"]).length; i++){
+			$("#NPCRegard").html(function(k, oldHTML){
+				//Modify the "NPC Regard" div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["NPC Regard"][i].name + "</div>" +
+					"<div class='item-text'>" + data["NPC Regard"][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
+		for(var i = 0; i< Object.keys(data["Misc."]).length; i++){
+			$("#Misc").html(function(k, oldHTML){
+				//Modify the "Misc." div to reflect the existing data, plus whatever is below.
+				return oldHTML + 
+				"<div class='item'>" +
+					"<div class='item-header'>" + data["Misc."][i].name + "</div>" +
+					"<div class='item-text'>" + data["Misc."][i].summary + "</div>" + 
+				"</div>"
+			});
+		};
 	});
 	hideSections();
 }
@@ -128,6 +208,7 @@ function openOverlays(){
 	$(document).ready(function(){
 		$(".item-header").click(function()
 		{
+			var containerName = this.parentNode.parentNode.parentNode.getAttribute("id");
 			var actionName = $(this).text().trim();
 			console.log(actionName);
 			$("#overlay").html("<h1>" + actionName + "</h1>");
@@ -137,60 +218,112 @@ function openOverlays(){
 				$("#overlayback").css("display", "flex");
 				//Fill overlay with content
 				
-				$.getJSON("/data/skillDetails.json", function (data){
-					var index = 0;
-					var keyFound = false;
-					var upperBound = Object.keys(data.Action).length;
-					while(keyFound == false && index < upperBound){
-						if(data.Action[index].name.trim() == actionName){
-							keyFound = true;
-						}
-						index++;
-					};
-					if(keyFound == true) {
-						index--;
-						//get paragraphs
-						var bodyText = "";
-						for(var j = 0; j < data.Action[index].text.length;j++){
-							bodyText += data.Action[index].text[j] + "<br>";
-						}
-						//get possible pieces
-							if(data.Action[index].trigger != "" && data.Action[index].trigger != undefined) { 
-								var triggerFound = "<b>Trigger: </b>" + data.Action[index].trigger + "<br>"; 
-							} else { var triggerFound = ""; };
-							if(data.Action[index].requirements != "" && data.Action[index].requirements != undefined) { 
-								var requirementFound = "<b>Requirements: </b>" + data.Action[index].requirements + "<br>"; 
-							} else { var requirementFound = ""; };
-							if(data.Action[index].critSuccess != "" && data.Action[index].critSuccess != undefined) { 
-								var critSuccessFound = "<b>Critical Success: </b>" + data.Action[index].critSuccess + "<br>"; 
-							} else { var critSuccessFound = ""; };
-							if(data.Action[index].success != "" && data.Action[index].success != undefined) { 
-								var successFound = "<b>Success: </b>" + data.Action[index].success + "<br>"; 
-							} else { var successFound = ""; };
-							if(data.Action[index].failure != "" && data.Action[index].failure != undefined) { 
-								var failureFound = "<b>Failure: </b>" + data.Action[index].failure + "<br>"; 
-							} else { var failureFound = ""; };
-							if(data.Action[index].critFailure != "" && data.Action[index].critFailure != undefined) { 
-								var critFailureFound = "<b>Critical Failure: </b>" + data.Action[index].critFailure + "<br>"; 
-							} else { var critFailureFound = ""; };
+				if(containerName != "ConditionsContainer") {
+					$.getJSON("/data/skillDetails.json", function (data){
+						var index = 0;
+						var keyFound = false;
+						var upperBound = Object.keys(data.Action).length;
+						while(keyFound == false && index < upperBound){
+							if(data.Action[index].name.trim() == actionName){
+								keyFound = true;
+							}
+							index++;
+						};
+						if(keyFound == true) {
+							index--;
+							//get paragraphs
+							var bodyText = "";
+							for(var j = 0; j < data.Action[index].text.length;j++){
+								bodyText += data.Action[index].text[j] + "<br>";
+							}
+							//get possible pieces
+								if(data.Action[index].trigger != "" && data.Action[index].trigger != undefined) { 
+									var triggerFound = "<b>Trigger: </b>" + data.Action[index].trigger + "<br>"; 
+								} else { var triggerFound = ""; };
+								if(data.Action[index].requirements != "" && data.Action[index].requirements != undefined) { 
+									var requirementFound = "<b>Requirements: </b>" + data.Action[index].requirements + "<br>"; 
+								} else { var requirementFound = ""; };
+								if(data.Action[index].critSuccess != "" && data.Action[index].critSuccess != undefined) { 
+									var critSuccessFound = "<b>Critical Success: </b>" + data.Action[index].critSuccess + "<br>"; 
+								} else { var critSuccessFound = ""; };
+								if(data.Action[index].success != "" && data.Action[index].success != undefined) { 
+									var successFound = "<b>Success: </b>" + data.Action[index].success + "<br>"; 
+								} else { var successFound = ""; };
+								if(data.Action[index].failure != "" && data.Action[index].failure != undefined) { 
+									var failureFound = "<b>Failure: </b>" + data.Action[index].failure + "<br>"; 
+								} else { var failureFound = ""; };
+								if(data.Action[index].critFailure != "" && data.Action[index].critFailure != undefined) { 
+									var critFailureFound = "<b>Critical Failure: </b>" + data.Action[index].critFailure + "<br>"; 
+								} else { var critFailureFound = ""; };
 
-						//actually set the overlay
-						$("#overlay").html(function(k, oldHTML){
-							return "" + 
-							"<div class='overlay-header'><a href='" + data.Action[index].aonlink + "'>" + data.Action[index].name + "</a></div>" +
-							"<div class='overlay-page'>p. " + data.Action[index].page + "</div>" +
-							"<div class='overlay-body'>" +
-								triggerFound +  requirementFound +
-								"<br>" + bodyText + "<br>" + 
-								critSuccessFound + successFound + failureFound + critFailureFound +
-							"</div>"
-						});
-					}
-					else {
-						//Entry not found in skillDetails, fill overlay with false detail.
-						$("#overlay").html("<h1> Entry not found. </h1>");
-					};
-				});
+							//actually set the overlay
+							$("#overlay").html(function(k, oldHTML){
+								return "" + 
+								"<div class='overlay-header'><a href='" + data.Action[index].aonlink + "'>" + data.Action[index].name + "</a></div>" +
+								"<div class='overlay-page'>p. " + data.Action[index].page + "</div>" +
+								"<div class='overlay-body'>" +
+									triggerFound +  requirementFound +
+									"<br>" + bodyText + "<br>" + 
+									critSuccessFound + successFound + failureFound + critFailureFound +
+								"</div>"
+							});
+						}
+						else {
+							//Entry not found in skillDetails, fill overlay with false detail.
+							$("#overlay").html("<h1> Entry not found. </h1>");
+						};
+					});
+				}
+				else if (containerName == "ConditionsContainer") {
+					//console.log("I haven't actually defined this yet, sorry");
+					$.getJSON("/data/Conditions.json", function (data){
+						var FLindex = 0;
+						var keyFound = false;
+						var upperBound = Object.keys(data.FullList).length;
+						while(keyFound == false && FLindex < upperBound){
+							if(data.FullList[FLindex].name.trim() == actionName){
+								keyFound = true;
+							}
+							FLindex++;
+						};
+						if(keyFound == true) {
+							FLindex--;
+							//We now have the Condition's position in the FullList array (FLindex).
+							var CType = data.FullList[FLindex].type;
+							//We now need to find the Condition's entry in the sub-array.
+							var index = 0;
+							var subkeyFound = false;
+							var subUpperBound = Object.keys(data[CType]).length;
+							while(subkeyFound == false && index < subUpperBound){
+								if(data[CType][index].name.trim() == actionName){
+									subkeyFound = true;
+								}
+								index++;
+							};
+							if(subkeyFound == true){
+								index--;
+								//We have the full reference! Populate the overlay.
+								var bodyText = "";
+								for(var i = 0; i < Object.keys(data[CType][index].text).length; i++){
+									bodyText += data[CType][index].text[i] + "<br>";
+								}
+								//actually set the overlay
+								$("#overlay").html(function(k, oldHTML){
+									return "" + 
+									"<div class='overlay-header'><a href='" + data[CType][index].aonlink + "'>" + data[CType][index].name + "</a></div>" +
+									"<div class='overlay-page'>p. " + data[CType][index].page + "</div>" +
+									"<div class='overlay-body'>" +
+										"<br>" + bodyText + "<br>" + 
+									"</div>"
+								});
+							}
+						}						
+						else {
+							//Entry not found in skillDetails, fill overlay with false detail.
+							$("#overlay").html("<h1> Entry not found. </h1>");
+						};
+					});
+				};
 			};
 		});
 		$("#overlayback").click(function(){
