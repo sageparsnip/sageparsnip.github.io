85790d21d4b64a025c32bb5390f71260a38b7c96
sageparsnip <123469117+sageparsnip@users.noreply.github.com>
2025-06-27 15:18:00 +0100

    Added password lock to main page content

--- Limmies-And-Andies-2025/scripts/main.js
+++ Limmies-And-Andies-2025/scripts/main.js
@@ -1,78 +1,31 @@
-function onLoadJSON(){
-	const characterInput = document.getElementById("characterInput");
-	const monsterInput = document.getElementById("monsterInput");
-	const encounterInput = document.getElementById("encounterInput");
-	const itemInput = document.getElementById("itemInput");
-	const spellInput = document.getElementById("spellInput");
-
-	characterInput.addEventListener("change", function () {
-		const file = characterInput.files[0];
-  		const reader = new FileReader();
-  		reader.addEventListener("load", function () {
-   			const data = JSON.parse(reader.result);
-    		localStorage.setItem("characterData", JSON.stringify(data));
-  		});
-  		reader.readAsText(file);
-	});
-	monsterInput.addEventListener("change", function () {
-		const file = monsterInput.files[0];
-  		const reader = new FileReader();
-  		reader.addEventListener("load", function () {
-   			const data = JSON.parse(reader.result);
-    		localStorage.setItem("monsterData", JSON.stringify(data));
-  		});
-  		reader.readAsText(file);
-	});
-	encounterInput.addEventListener("change", function () {
-		const file = encounterInput.files[0];
-  		const reader = new FileReader();
-  		reader.addEventListener("load", function () {
-   			const data = JSON.parse(reader.result);
-    		localStorage.setItem("encounterData", JSON.stringify(data));
-  		});
-  		reader.readAsText(file);
-	});
-	itemInput.addEventListener("change", function () {
-		const file = itemInput.files[0];
-  		const reader = new FileReader();
-  		reader.addEventListener("load", function () {
-   			const data = JSON.parse(reader.result);
-    		localStorage.setItem("itemData", JSON.stringify(data));
-  		});
-  		reader.readAsText(file);
-	});
-	spellInput.addEventListener("change", function () {
-		const file = spellInput.files[0];
-  		const reader = new FileReader();
-  		reader.addEventListener("load", function () {
-   			const data = JSON.parse(reader.result);
-    		localStorage.setItem("spellData", JSON.stringify(data));
-  		});
-  		reader.readAsText(file);
-	});
-}
-
-function interrogateJSON() {
-	characterData = JSON.parse(localStorage.getItem("characterData"));
-	console.log(characterData["characters"]);
-	for(let i = 0; i < characterData["characters"].length; i++){
-		console.log("Character name at position " + i + ": " + characterData["characters"][i]["Name"]);
-	}
-}
-
 document.addEventListener("DOMContentLoaded", () => { //JS On-Ready! Fire start-up commands here.
 	//Put in what I need to do on loading page here!
+	ShowPasswordInput();
 });
 
-function exportJSON(dataType) {
-	const jsonString = localStorage.getItem(dataType);
-	const outputObject = new Blob([jsonString], { type: 'application/json'});
-	const download = document.createElement('a');
-	download.href = URL.createObjectURL(outputObject);
-	download.download = `DND5eDataSilo - ${dataType}.json`;
-	document.body.appendChild(download);
-	download.click();
-	console.log(dataType + ".JSON download initiated");
+function ShowPasswordInput(){
+	var modal = document.getElementById("diceRollPopup");
+	var modalHeader = document.getElementById("diceRollPopupHeader");
+	var modalContent = document.getElementById("diceRollPopupContent");
+	var mainPageContent = document.getElementById("mainPageContent");
+	mainPageContent.style.display = 'none';
+	modalHeader.innerHTML = 'Input password to show page';
+	modalContent.innerHTML = `
+		<input type="text"></input><button onclick="CheckPass(this)">Confirm</button>
+	`;
+	modal.style.display = "block";
+}
+
+function CheckPass(div){
+	var pass = div.parentNode.childNodes[1].value;
+	if(pass === 'ItsOnlyGame'){
+		var mainPageContent = document.getElementById("mainPageContent");
+		mainPageContent.style.display = 'block';
+		hideModal();
+	}
+	else{
+		div.parentNode.childNodes[1].value = 'Incorrect!';
+	}
 }
 
 function hideModal() {
